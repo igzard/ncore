9bcaa1ea20223dcf9c11d6e869e0a96c4a9378d5
Ignácz Gergely <gergely.igncz@gmail.com>
2024-04-10 19:13:39 +0200

    feat: init client and get hitnrun

---
+++ tests/NcoreClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class NcoreClientTest extends TestCase
+{
+    public function testClient(): void
+    {
+        $this->assertTrue(true);
+    }
+}
